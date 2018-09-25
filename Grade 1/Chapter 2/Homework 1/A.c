a068e043a97294f07d12332e41e0cdeea6903199
HeRaNO <heran55@126.com>
2018-09-25 18:36:14 +0800

    Chapter 2

---
+++ Grade 1/Chapter 2/Homework 1/A.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(void)
+{
+	puts("To C, or not to C: that is a question.");
+	return 0;
+}
---
+++ Grade 1/Chapter 2/Homework 1/B.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+	puts("Did you have a heavy dinner last night?");
+	return 0;
+}
