d092c2adf16c20c28cc300d2c9fe5be6b88ff901
HeRaNO <heran55@126.com>
2018-11-19 22:47:49 +0800

    Chapter 11

--- CourseGrading/Grade 1/Chapter 11/C.c
+++ CourseGrading/Grade 1/Chapter 11/C.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
-int *find_largest(int a[],int n,int *mx)
+int *find_largest(int a[],int n)
 {
-	int i,t;*mx=1<<31;
-	for (i=0;i<n;i++) if (*mx<a[i]){*mx=a[i];t=i;}
-	return &a[t];
+	int i;int *mx=&a[0];
+	for (i=0;i<n;i++) if (*mx<a[i]) mx=&a[i];
+	return mx;
 }
 
 int main()
 {
-	int a[10],mx,i;int *p;
+	int a[10],i;int *p;
 	printf("Enter 10 integers:\n");
 	for (i=0;i<10;i++) scanf("%d",&a[i]);
-	p=find_largest(a,10,&mx);
+	p=find_largest(a,10);
 	printf("The largest: %d\n",*p);
 	return 0;
 }
