6166b78f47380352ea24f044e4ae24680731a55a
HeRaNO <heran55@126.com>
2018-11-26 13:12:43 +0800

    Experiment 4

--- OSETC@ISE/Experiment 4/Q1.c
+++ OSETC@ISE/Experiment 4/Q1.c
@@ -7,9 +7,9 @@ int n,i,top,f;
 
 int main()
 {
-	printf("Enter parenteses and/or braces: ");
+	printf("Enter parentheses and/or braces: ");
 	scanf("%s",s);n=strlen(s);
-	for (i=0;i<n;i++)
+	for (i=0;i<n&&!f;i++)
 	{
 		if (s[i]=='('||s[i]=='{') sta[++top]=s[i];
 		else
@@ -19,14 +19,10 @@ int main()
 				if (s[i]=='}'&&sta[top]=='{') --top;
 				if (s[i]==')'&&sta[top]=='(') --top;
 			}
-			else
-			{
-				puts("Stack overflow");f=1;
-				break;
-			}
+			else f=1;
 		}
 	}
-	if (top||f) puts("Parenteses/braces are not nested properly");
-	else puts("Parenteses/braces are nested properly");
+	if (top||f) puts("Parentheses/braces are NOT nested properly");
+	else puts("Parentheses/braces are nested properly");
 	return 0;
 }
